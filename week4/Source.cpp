c618ffcf6abdcf65617af7a9eb4d27396ac135fa
Parncncd <69336494+Parncncd@users.noreply.github.com>
2020-09-05 12:49:44 +0700

    Add project files.

---
+++ week4/Source.cpp
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int squareSum(int x) {
+	int sum = 0;
+	for (int i = 1; i <= x; i++) {
+		sum += i;
+
+	}
+	x = sum * sum;
+	return x;
+}
+int sumSquare(int y) {
+	int sum = 0;
+	for (int i = 1; i <= y; i++) {
+		sum += i * i;
+
+	}
+	return sum;
+}
+int main() {
+	int num = 100;
+	printf("%d", squareSum(num) - sumSquare(num));
+	return 0;
+}
